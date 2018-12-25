a8e1de2544d629573c43393f95f9a2282bdb740a
Patrick Löber <patrickloeber90@googlemail.com>
2018-12-25 18:43:46 +0100

    Initial Commit

---
+++ ScrollDatePicker/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ScrollDatePicker
+//
+//  Created by Patrick Löber on 25.12.18.
+//  Copyright © 2018 Patrick Loeber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
+    }
+
+
+}
+
